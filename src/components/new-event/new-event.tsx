49342c252b0ef7035c5d034834272f877afed9b4
Esteve <esteve.17mas@gmail.com>
2024-12-26 22:28:16 +0100

    feat: useSearchParamsManager custom hook

--- src/components/new-event/new-event.tsx
+++ src/components/new-event/new-event.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import {
   EventTypeBox,
@@ -18,39 +18,26 @@ import {
   EventType,
   IEventFields,
 } from "./new-event.interface";
-import { CustomButton, Modal } from "../base";
-import { useSearchParams } from "react-router-dom";
 import { useCreateClass } from "../../api";
-import { useClickOutside } from "../../hooks";
+import { useClickOutside, useSearchParamsManager } from "../../hooks";
+import { CustomButton, Modal } from "../base";
 
 export const NewEvent = () => {
   const { t } = useTranslation();
-  const [searchParams, setSearchParams] = useSearchParams();
+  const { params, setParams } = useSearchParamsManager(["type"]);
 
   const [fields, setFields] = useState<IEventFields>(emptyEventFields);
 
-  const { mutate, isPending: isLoading } = useCreateClass();
-
   const handleCloseModal = () => {
-    setSearchParams((sParams) => {
-      sParams.delete("type");
-      sParams.delete("action");
-      return sParams;
-    });
+    setParams([{ key: "type" }, { key: "action" }]);
   };
+
+  const { mutate, isPending: isLoading } = useCreateClass(handleCloseModal);
 
   const ref = useClickOutside(handleCloseModal);
 
-  const selectedType: EventType | null = useMemo(() => {
-    const type = searchParams.get("type");
-    return type ? (type as EventType) : null;
-  }, [searchParams]);
-
   const handleSelectType = (newType?: EventType) => {
-    setSearchParams((sParams) => {
-      newType ? sParams.set("type", newType) : sParams.delete("type");
-      return sParams;
-    });
+    setParams([{ key: "type", value: newType }]);
   };
 
   const handleSubmit = () => {
@@ -74,7 +61,7 @@ export const NewEvent = () => {
         <span className="text-center text-2xl font-bold">
           {t("Calendar.Event.NewEvent")}
         </span>
-        {!selectedType ? (
+        {!params.get("type") ? (
           <EventTypesWrapper>
             <EventTypeBox
               type="Recurrent"
@@ -90,7 +77,7 @@ export const NewEvent = () => {
         ) : (
           <EventFormWrapper>
             <InputFieldsContainer>
-              {selectedType === "recurrent" ? (
+              {params.get("type") === "recurrent" ? (
                 <RecurrentFields fields={fields} setFields={setFields} />
               ) : (
                 <OneTimeFields fields={fields} setFields={setFields} />
---
+++ src/hooks/search-params-manager.ts
@@ -0,0 +1,29 @@
+import { useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
+
+export interface IChangeParams {
+  key: string;
+  value?: string;
+}
+
+export function useSearchParamsManager(params: string[]) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const paramsValues = useMemo(() => {
+    return new Map<string, string | null>(
+      params.map((item) => [item, searchParams.get(item)])
+    );
+  }, [params, searchParams]);
+
+  const handleChangeParams = (params: IChangeParams[]) => {
+    setSearchParams((sParams) => {
+      params.forEach(({ key, value }) => {
+        if (!value) sParams.delete(key);
+        else sParams.set(key, value);
+      });
+      return sParams;
+    });
+  };
+
+  return { params: paramsValues, setParams: handleChangeParams };
+}
