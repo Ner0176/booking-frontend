c0114f5c928caff282ec6c753fe1d68e2ddc9461
Esteve <esteve.17mas@gmail.com>
2025-02-09 08:59:42 +0100

    feat: allow cookies + forgive access if user has not valid cookie

--- src/api/axios-instance.ts
+++ src/api/axios-instance.ts
@@ -1,8 +1,19 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
+  withCredentials: true,
   baseURL: "http://localhost:8000/",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
