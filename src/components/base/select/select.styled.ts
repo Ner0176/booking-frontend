c411b9433f7ff487397864f0ec9f014bac42adb5
Esteve <esteve.17mas@gmail.com>
2025-02-18 18:18:37 +0100

    style: fix select not displaying whole text

--- src/components/base/select/select.styled.ts
+++ src/components/base/select/select.styled.ts
@@ -5,5 +5,5 @@ export const SelectContainer = styled.div(
   tw`flex flex-row items-center justify-between w-full rounded-xl border border-neutral-200`
 );
 export const StyledSelect = styled.select(
-  tw`py-2 px-4 text-sm w-full rounded-xl !outline-none appearance-none bg-white cursor-pointer`
+  tw`py-2 px-4 text-sm  rounded-xl !outline-none appearance-none bg-white cursor-pointer`
 );
