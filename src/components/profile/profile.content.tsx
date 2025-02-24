80c173da853961d14a97328ec6c7fc3bafdbeab8
Esteve <esteve.17mas@gmail.com>
2025-02-24 17:08:14 +0100

    style: profile mobile styles

--- src/components/profile/profile.content.tsx
+++ src/components/profile/profile.content.tsx
@@ -1,4 +1,3 @@
-import Skeleton from "react-loading-skeleton";
 import { IUser, useUpdateUser } from "../../api";
 import { useSearchParamsManager, useUser } from "../../hooks";
 import { useTranslation } from "react-i18next";
@@ -12,35 +11,6 @@ import { CustomButton, CustomInputField, LanguageSelector } from "../base";
 import { mdiAccountOutline, mdiEmailOutline, mdiPhoneOutline } from "@mdi/js";
 import { checkPhone } from "../../utils";
 import { UserInfoField } from "../users";
-
-const LateralSkeleton = () => {
-  return (
-    <div className="flex flex-col gap-4">
-      <Skeleton style={{ width: 150, height: 25 }} />
-      {[...Array(4)].map((_, idx) => (
-        <Skeleton
-          key={idx}
-          style={{ width: "100%", height: 35, borderRadius: 16 }}
-        />
-      ))}
-    </div>
-  );
-};
-
-export const ProfileLoadingSkeleton = ({
-  isLoading,
-}: Readonly<{ isLoading: boolean }>) => {
-  return (
-    <div className="grid grid-cols-3 gap-10">
-      <LateralSkeleton />
-      <div className="flex flex-col gap-3">
-        <Skeleton style={{ width: "100%", height: 35, borderRadius: 16 }} />
-        <Skeleton style={{ width: "100%", height: 500, borderRadius: 16 }} />
-      </div>
-      <LateralSkeleton />
-    </div>
-  );
-};
 
 export const EditProfileInformation = ({
   user,
