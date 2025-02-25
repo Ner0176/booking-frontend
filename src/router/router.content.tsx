320fded3441bb9b71e47cc6225cae287130dbb47
Esteve <esteve.17mas@gmail.com>
2025-02-25 22:08:57 +0100

    feat: router conditional home + sidebar layouts

--- src/router/router.content.tsx
+++ src/router/router.content.tsx
@@ -2,6 +2,8 @@ import { Navigate, Outlet } from "react-router-dom";
 import { InvalidRouteContainer } from "./router.styled";
 import { useTranslation } from "react-i18next";
 import { useUser } from "../stores";
+import { isMobile } from "react-device-detect";
+import { CalendarDashboard, MobileSidebar, Sidebar } from "../components";
 
 export const InvalidRoute = () => {
   const { t } = useTranslation();
@@ -11,6 +13,29 @@ export const InvalidRoute = () => {
       <div className="text-2xl">{t("Auth.InvalidRoute.Description")}</div>
     </InvalidRouteContainer>
   );
+};
+
+export const SidebarLayout = () => {
+  const user = useUser();
+
+  if (isMobile) {
+    return (
+      <MobileSidebar>
+        {!user ? <Navigate to={"/login"} /> : <Outlet />}
+      </MobileSidebar>
+    );
+  }
+  return <Sidebar>{!user ? <Navigate to={"/login"} /> : <Outlet />}</Sidebar>;
+};
+
+export const ConditionalHome = () => {
+  const user = useUser();
+
+  if (isMobile) {
+    return <Navigate to={!user?.isAdmin ? "/bookings" : "/management"} />;
+  }
+
+  return <CalendarDashboard />;
 };
 
 export const ProtectedRoute = ({
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,38 +1,28 @@
 import {
   AuthForm,
-  CalendarDashboard,
   ClassesManagementDashboard,
   PoliciesDashboard,
   ProfileDashboard,
   SettingsDashboard,
-  Sidebar,
   UserBookingsDashboard,
   UsersDashboard,
 } from "../components";
-import { useUser } from "../hooks";
-import { InvalidRoute, ProtectedRoute } from "./router.content";
 import {
-  BrowserRouter,
-  Navigate,
-  Outlet,
-  Route,
-  Routes,
-} from "react-router-dom";
+  ConditionalHome,
+  InvalidRoute,
+  ProtectedRoute,
+  SidebarLayout,
+} from "./router.content";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export const AppRouter = () => {
-  const { user } = useUser();
-
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AuthForm type="Login" />} path="/login" />
         <Route element={<AuthForm type="SignUp" />} path="/register" />
-        <Route
-          element={
-            <Sidebar>{!user ? <Navigate to={"/login"} /> : <Outlet />}</Sidebar>
-          }
-        >
-          <Route path="/" element={<CalendarDashboard />} />
+        <Route element={<SidebarLayout />}>
+          <Route path="/" element={<ConditionalHome />} />
           <Route path="/profile" element={<ProfileDashboard />} />
           <Route path="/policies" element={<PoliciesDashboard />} />
 
