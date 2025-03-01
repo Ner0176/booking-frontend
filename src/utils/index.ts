abd602b5fd44240e840efb13bb01c244978c7653
Esteve <esteve.17mas@gmail.com>
2025-03-01 15:30:05 +0100

    feat: includes string function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,6 @@ export * from "./capitalize";
 export * from "./check-phone";
 export * from "./format-time";
 export * from "./get-weekday";
+export * from "./string-includes";
 export * from "./merge-date-time";
 export * from "./format-long-date";
