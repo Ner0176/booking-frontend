af85bcccef343a066f4c3f35e4ce24483b33b3d6
Esteve <esteve.17mas@gmail.com>
2025-03-14 20:44:04 +0100

    fix: correct env variable nomenclature

--- src/api/axios-instance.ts
+++ src/api/axios-instance.ts
@@ -5,7 +5,7 @@ export const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:8000"
-      : "https://mioreset-app-backend.onrender.com",
+      : process.env.REACT_APP_BACK_PROD_URL,
   headers: {
     "Content-Type": "application/json",
   },
